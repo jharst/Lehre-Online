d0894a448b76f79616292cbe26c5a2a0a2eec9eb
Joachim Harst <jharst@posteo.de>
2025-01-28 12:25:04 +0100

    KW5 Lehre fertig

--- hideHashtags.js
+++ hideHashtags.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('div').forEach(div => {
         div.innerHTML = div.innerHTML.replace(/#(\w+)/g, '<span class="hidden-tag">#$1</span>');
+        div.innerHTML = div.innerHTML.replace(/(\[\"\])/g, '<span class="hidden-tag">$1</span>');
     });
 });
