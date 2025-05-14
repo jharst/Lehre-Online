0613011237506a763827546da16a03d5aeb0a3b9
Joachim Harst <jharst@posteo.de>
2025-05-14 10:04:41 +0200

    KW19 6 Don Quijote

--- quoteBullets.js
+++ quoteBullets.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('ul.embedded-quote-block').forEach(ul => {
         ul.classList.add('fa-ul');
         ul.querySelectorAll('li').forEach(li => {
-            li.innerHTML = li.innerHTML.replace(/\[\"\]/g, '<i class="fa-li fa fa-quote-right"></i>');
+            li.innerHTML = li.innerHTML.replace(/\[\"\]/g, '<i class="fa-li fa fa-quote-right" style="color: #772F8B75; font-size: smaller;"></i>');
         });
     });
 });
