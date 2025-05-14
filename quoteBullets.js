ec6072383c8808130c229368c672a97ad055d4e3
Joachim Harst <jharst@posteo.de>
2025-05-14 13:48:28 +0200

    KW19 6 Don Quijote/QuoteBullets.js fix

--- quoteBullets.js
+++ quoteBullets.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('ul.embedded-quote-block').forEach(ul => {
-        ul.classList.add('fa-ul');
-        ul.querySelectorAll('li').forEach(li => {
-            li.innerHTML = li.innerHTML.replace(/\[\"\]/g, '<i class="fa-li fa fa-quote-right" style="color: #772F8B75; font-size: smaller;"></i>');
-        });
+    document.querySelectorAll('ul').forEach(ul => {
+        //Bearbeite alle ul-Elemente, die ["] beinhalten, füge ihnen die Klassen 'fa-ul' und 'embedded-quote-block' hinzu
+        //und lösche alle Tags (aber nicht style-Anweisungen wie 'color: #772F8B75')
+        
+        if (ul.innerHTML.includes('["]')) {
+            ul.classList.add('fa-ul');
+            ul.classList.add('embedded-quote-block');
+            ul.querySelectorAll('li').forEach(li => {
+                li.innerHTML = li.innerHTML.replace(/\[\"\]/g, '<i class="fa-li fa fa-quote-right" style="color: #772F8B75; font-size: smaller;"></i>');
+                li.innerHTML = li.innerHTML.replace(/(?<!")(#[a-zA-Z_]+\b)(?!")/g, '');
+            });
+        };
     });
 });
