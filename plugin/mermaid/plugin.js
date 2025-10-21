bf6b49b5af941828f6c7c05363050831a73c8d1b
Joachim Harst <jharst@posteo.de>
2025-10-21 12:02:05 +0200

    KW43 Lehre + update mermaid.js

--- plugin/mermaid/plugin.js
+++ plugin/mermaid/plugin.js
@@ -2,16 +2,63 @@
  * reveal.js Mermaid plugin
  */
 
-import mermaid from "mermaid";
+import mermaid from "mermaid/dist/mermaid.esm.mjs";
+
+async function renderMermaid({ el, beforeRender, afterRender }) {
+  const beforeRenderRes = await beforeRender?.(el);
+
+  if (beforeRenderRes === false) {
+    return;
+  }
+
+  // Using textContent not innerHTML, because innerHTML will get escaped code (eg: get --&gt; instead of -->).
+  const graphDefinition = el.textContent.trim();
+
+  try {
+    const { svg: svgCode } = await mermaid.render(
+      `mermaid-${Math.random().toString(36).substring(2)}`,
+      graphDefinition
+    );
+    el.innerHTML = svgCode;
+
+    await afterRender?.(el);
+  } catch (error) {
+    let errorStr = "";
+    if (error?.str) {
+      // From mermaid 9.1.4, error.message does not exists anymore
+      errorStr = error.str;
+    }
+    if (error?.message) {
+      errorStr = error.message;
+    }
+    console.error(errorStr, { error, graphDefinition, el });
+    el.innerHTML = errorStr;
+  }
+}
+
+function getRenderMermaidEl({ beforeRender, afterRender }) {
+  return function renderMermaidEl(el) {
+    return renderMermaid({
+      el,
+      beforeRender,
+      afterRender,
+    });
+  };
+}
 
 const Plugin = {
   id: "mermaid",
 
   init: function (reveal) {
+    const { ...mermaidConfig } = reveal.getConfig().mermaid || {};
+    const { ...mermaidPluginConfig } = reveal.getConfig().mermaidPlugin || {};
 
-    let { ...mermaidConfig } = reveal.getConfig().mermaid || {};
+    const renderMermaidEl = getRenderMermaidEl({
+      beforeRender: mermaidPluginConfig.beforeRender,
+      afterRender: mermaidPluginConfig.afterRender,
+    });
 
-    mermaid.mermaidAPI.initialize({
+    mermaid.initialize({
       // The node size will be calculated incorrectly if set `startOnLoad: start`,
       // so we need to manually render.
       startOnLoad: false,
@@ -21,30 +68,7 @@ const Plugin = {
     const mermaidEls = reveal.getRevealElement().querySelectorAll(".mermaid");
 
     Array.from(mermaidEls).forEach(function (el) {
-      var insertSvg = function (svgCode, bindFunctions) {
-        el.innerHTML = svgCode;
-      };
-
-      var graphDefinition = el.textContent.trim();
-
-      try {
-        mermaid.mermaidAPI.render(
-          `mermaid-${Math.random().toString(36).substring(2)}`,
-          graphDefinition,
-          insertSvg
-        );
-      } catch (error) {
-        let errorStr = "";
-        if (error?.str) {
-          // From mermaid 9.1.4, error.message does not exists anymore
-          errorStr = error.str;
-        }
-        if (error?.message) {
-          errorStr = error.message;
-        }
-        console.error(errorStr, { error, graphDefinition, el });
-        el.innerHTML = errorStr;
-      }
+      renderMermaidEl(el);
     });
   },
 };
